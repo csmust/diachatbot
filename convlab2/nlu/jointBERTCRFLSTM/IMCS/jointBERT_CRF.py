376b83fc4f040ede8208fc044fee153d6e2dcae6
周子涵 <1226674224@qq.com>
2023-07-03 23:17:48 +0800

    softmax单意图

--- convlab2/nlu/jointBERTCRFLSTM/IMCS/jointBERT_CRF.py
+++ convlab2/nlu/jointBERTCRFLSTM/IMCS/jointBERT_CRF.py
@@ -306,8 +306,8 @@ class JointBERTCRFLSTM(nn.Module):
             #     key_dim=self.bert.config.hidden_size,
             #     value_dim=self.bert.config.hidden_size)
         # self.myMultiattention = MultiHeadAttention(self.bert.config.hidden_size)
-        self.intent_loss_fct = torch.nn.BCEWithLogitsLoss(pos_weight=self.intent_weight)
-
+        # self.intent_loss_fct = torch.nn.BCEWithLogitsLoss(pos_weight=self.intent_weight)
+        self.intent_loss_fct = torch.nn.CrossEntropyLoss()
         if LAST_ADD_CRF:
             print("----BERT+CRF------")
             print("----请注意核对后处理.py 以及 train.py的model.forward()使用-------")
